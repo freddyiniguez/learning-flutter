a28998ccb021570e5eb21f849b50d76a5f6d5df5
Freddy Iniguez <Freddy Iniguez>
2019-05-09 13:35:10 -0500

    Finishing section 3 about debugging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,35 @@
 // NOTE: Dart style guide:
 // https://www.dartlang.org/guides/language/effective-dart/style
 
+// NOTE: Flutter is
+// 1) An SDK
+// 2) A Dart Framework
+
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
-main() => runApp(MyApp());
+// NOTE: Use the following syntax when there is just a line of code.
+// main() => runApp(MyApp());
+
+main() {
+  // NOTE: The following lines of code allow us to debug the UI.
+  // Rendering package needs to be imported.
+  //debugPaintSizeEnabled = true;
+  //debugPaintBaselinesEnabled = true;
+  //debugPaintPointersEnabled = true;
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   /// Function: Build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        // NOTE: The following line is used to debug UI.
+        // debugShowMaterialGrid: true,
         theme: ThemeData(
             brightness: Brightness.light,
             primarySwatch: Colors.deepOrange,
